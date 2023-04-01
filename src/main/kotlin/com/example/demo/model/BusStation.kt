159af49fa5f55f352e6acb6f55cf741e8143e2b0
Asad Raza <asad.raza@sap.com>
2023-04-01 22:47:11 +0200

    Added KDoc for each service of Bus Station

--- src/main/kotlin/com/example/demo/model/BusStation.kt
+++ src/main/kotlin/com/example/demo/model/BusStation.kt
@@ -1,8 +1,11 @@
 package com.example.demo.model
 
 import javax.persistence.*
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
+import javax.validation.constraints.Pattern
 
 @Entity
 data class BusStation(
@@ -13,12 +16,15 @@ data class BusStation(
     val name: String,
 
     @NotNull
+    @Min(-90) @Max(90)
     val latitude: Double,
 
     @NotNull
+    @Min(-180) @Max(180)
     val longitude: Double,
 
     @NotBlank
+    @Pattern(regexp = "^[A-Z]{2}$", message = "Country code must consist of exactly 2 capital letters")
     @Column(length = 2)
     val countryCode: String
 )
--- src/main/kotlin/com/example/demo/service/BusStationService.kt
+++ src/main/kotlin/com/example/demo/service/BusStationService.kt
@@ -12,17 +12,48 @@ import kotlin.math.sqrt
 @Service
 class BusStationService(private val busStationRepository: BusStationRepository) {
 
-
+    /**
+     * Returns a list of all BusStations.
+     */
     fun findAll(): List<BusStation> = busStationRepository.findAll()
 
+    /**
+     * Returns a BusStation with the specified ID, or null if not found.
+     *
+     * @param id The ID of the BusStation to find.
+     */
     fun findById(id: Long): BusStation? = busStationRepository.findById(id).orElse(null)
 
+    /**
+     * Saves a new BusStation or updates an existing one.
+     *
+     * @param busStation The BusStation to save or update.
+     */
     fun save(busStation: BusStation): BusStation = busStationRepository.save(busStation)
 
+    /**
+     * Deletes a BusStation with the specified ID.
+     *
+     * @param id The ID of the BusStation to delete.
+     */
     fun deleteById(id: Long) = busStationRepository.deleteById(id)
 
+    /**
+     * Returns a list of BusStations with the specified country code.
+     *
+     * @param countryCode The country code to filter BusStations by.
+     */
     fun findByCountryCode(countryCode: String): List<BusStation> = busStationRepository.findByCountryCode(countryCode)
 
+
+    /**
+     * Returns a list of BusStations within the specified radius (in Km) of the given latitude and longitude.
+     *
+     * @param latitude The latitude to center the search around.
+     * @param longitude The longitude to center the search around.
+     * @param radius The radius (in kilometers) within which to search for BusStations.
+     * @return A list of BusStations within the specified radius.
+     */
     fun findWithinRadius(latitude: Double, longitude: Double, radius: Double): List<BusStation> {
         return busStationRepository.findAll().filter { busStation ->
             val distance = distance(busStation.latitude, busStation.longitude, latitude, longitude)
@@ -30,13 +61,33 @@ class BusStationService(private val busStationRepository: BusStationRepository) 
         }
     }
 
+    /**
+     * Computes the distance between two sets of latitude and longitude using the Haversine formula.
+     *
+     * @param lat1 The latitude of the first point.
+     * @param lon1 The longitude of the first point.
+     * @param lat2 The latitude of the second point.
+     * @param lon2 The longitude of the second point.
+     * @return The distance (in kilometers) between the two points.
+     */
     private fun distance(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Double {
         val earthRadius = 6371.0 // km
-        val dLat = Math.toRadians(lat2 - lat1)
-        val dLon = Math.toRadians(lon2 - lon1)
-        val a = sin(dLat / 2) * sin(dLat / 2) + cos(Math.toRadians(lat1)) * cos(Math.toRadians(lat2)) * sin(dLon / 2) * sin(dLon / 2)
+        val diffLat = Math.toRadians(lat2 - lat1)
+        val diffLon = Math.toRadians(lon2 - lon1)
+        val a = haversine(diffLat) + cos(Math.toRadians(lat1)) * cos(Math.toRadians(lat2)) * haversine(diffLon)
         val c = 2 * atan2(sqrt(a), sqrt(1 - a))
         return earthRadius * c
+    }
+
+    /**
+     * Calculates the haversine of an angle.
+     *
+     * @param angle The angle (in radians) for which to calculate the haversine.
+     * @return The haversine of the given angle.
+     */
+    private fun haversine(angle: Double): Double {
+        val sinHalfAngle = sin(angle / 2)
+        return sinHalfAngle * sinHalfAngle
     }
 
 }
